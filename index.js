e6c0537dd89fea629bb3784c05d5b32d8b899028
Ivosin <81767797+Ivosin@users.noreply.github.com>
2022-07-30 06:58:55 +0100

    updated scripts

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // let's begin...
+  alert("Welcome to my first JavaScript enabled page");
+      console.log("My name is Isaac-Victor Shonowo");
+let myFullName = 'Isaac-Victor Shonowo';
+let myHeight = '170cm';
+let myCountry = 'Nigeria';
+
+console.log(myFullName)
+console.log(myHeight)
+console.log(myCountry)
